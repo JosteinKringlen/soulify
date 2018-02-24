bd1dbb801ac5d3989cd81b27730ec0705b688051
andrmos <mossige.a@gmail.com>
2018-02-24 16:51:34 +0100

    Create method to fetch spotofy access token

--- src/spotifyApi.js
+++ src/spotifyApi.js
@@ -1,5 +1,29 @@
 const Spotify = require('spotify-web-api-js');
 const process = require('process');
+const fetch = require('node-fetch');
+const btoa = require('btoa');
 
-const clientId = process.env.SOULIFY_CLIENT_ID;
-const clientSecret = process.env.SOULIFY_CLIENT_SECRET;
+getAccessToken()
+
+function getAccessToken() {
+  const clientId = process.env.SOULIFY_CLIENT_ID;
+  const clientSecret = process.env.SOULIFY_CLIENT_SECRET;
+  const spotifyAuthUrl = 'https://accounts.spotify.com/api/token';
+
+  const bla = clientId + ':' + clientSecret;
+  const base64Creds = btoa(bla)
+  const auth = 'Basic ' + base64Creds;
+
+  const content = {
+    method: 'POST',
+    body: JSON.stringify({grant_type: 'client_credentials'}),
+    headers: {
+      'Authorization': auth
+    }
+  };
+
+  fetch(spotifyAuthUrl, content)
+    .then(res => res())
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+}
