b130021dbcf29949a3ba4a5a2ee3bbe541f8327e
Jostein Kringlen <josteinkringlen@gmail.com>
2018-02-24 18:58:51 +0100

    Iterating through playlist.json and downloading all un-downloaded files now work

--- src/soulify.js
+++ src/soulify.js
@@ -1,27 +1,52 @@
-//import slsk from 'slsk-client'
-//import process from 'process'
 const slsk = require('slsk-client');
 const process = require('process');
+let playlistsToDownload = require('../playlists');
+let clientGlobal;
 
 slsk.connect({
     user: process.env.SLSK_USER,
     pass: process.env.SLSK_PASS
 }, (err, client) => {
-    client.search({
-        req: 'nora en pure waves',
-        timeout: 2000
-    },(err, res) => {
-        if (err) console.log('Error:\n' + err);
-        res = res.sort((a, b) => b.size - a.size || b.speed - a.speed);
-        res = res.filter(track => track.slots);
-        res = res.filter(track => track.bitrate === 320);
-        console.log('Res:\n' + JSON.stringify(res[0]));
-
-        client.download({
-            file: res[0],
-            path: __dirname + '/random.mp3'
-        },(err, data) => {
-            if (err) console.log('Error:\n' + err);
-        })
-    })
+    clientGlobal = client;
+    for(let i = 0; i < playlistsToDownload.playlists.length; i++){
+        searchForSongsInPlaylist(playlistsToDownload.playlists[i].elements, i)
+    }
 });
+
+
+
+function searchForSongsInPlaylist(playlist, playListIndex) {
+    for (let i = 0; i < playlist.length; i++){
+        if (!playlist[i].downloaded){
+            clientGlobal.search({
+                req: `${playlist[i].artist} ${playlist[i].song}`,
+                timeout: 2000
+            }, (err, res) => {
+                if (err) console.log('Error:\n' + err);
+                res = res.sort((a, b) => {return b.size - a.size || b.speed - a.speed});
+                res = res.filter(track => track.slots);
+                res = res.filter(track => track.bitrate === 320);
+                const songToDownload = res[0];
+                console.log('Res:\n' + JSON.stringify(songToDownload));
+
+                let name = songToDownload.file.split("\\");
+                console.log(name[name.length-1]);
+
+                downloadSong(songToDownload, name[name.length-1], playListIndex, i);
+            })
+        }
+    }
+}
+
+
+function downloadSong(songToDownload, name, playListIndex, songIndex) {
+    console.log("playListIndex: " + playListIndex);
+    console.log("songIndex: " + songIndex);
+    clientGlobal.download({
+        file: songToDownload,
+        path: __dirname + `/${name}`
+    },(err, data) => {
+        if (err) console.log('Error:\n' + err);
+        console.log(data);
+    });
+}
