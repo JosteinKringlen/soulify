4b811cc6026176f5127404149f318ef82ef38f35
andrmos <mossige.a@gmail.com>
2018-02-24 14:16:15 +0100

    Create file for spotify api

---
+++ src/spotifyApi.js
@@ -0,0 +1,5 @@
+const Spotify = require('spotify-web-api-js');
+const process = require('process');
+
+const clientId = process.env.SOULIFY_CLIENT_ID;
+const clientSecret = process.env.SOULIFY_CLIENT_SECRET;
